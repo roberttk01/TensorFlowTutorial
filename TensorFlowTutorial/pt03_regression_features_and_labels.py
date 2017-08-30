c78f1188379236090d8518e7f9961c84b4c3d7da
roberttk01 <roberttk01@gmail.com>
2017-08-29 23:54:54 -0500

    Updated requirements.txt and completed parts 2 thru 6

---
+++ TensorFlowTutorial/pt03_regression_features_and_labels.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import math
+import quandl as Quandl
+
+df = Quandl.get('WIKI/GOOGL')
+df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+
+forecast_col = 'Adj. Close'
+df.fillna(-99999, inplace=True)
+
+forecast_out = int(math.ceil(0.01 * len(df))) # Forecasting out 1% of length of the dataframe
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
+print(df.head())
+
